5bd475b7c7452b708dbd4d1e08af7a3928f47434
Shawn KIM <shawn@mstore.local>
2018-05-03 10:39:44 +0900

    -세번째 커밋

--- AccenAlert/AccenAlert/AppDelegate.m
+++ AccenAlert/AccenAlert/AppDelegate.m
@@ -16,6 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    NSLog(@"세번째  로그");
+
     // Override point for customization after application launch.
     return YES;
 }
