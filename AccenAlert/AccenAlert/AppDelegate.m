870dede43003eca4d1fc5b887b907c20529a186d
Shawn KIM <shawn@mstore.local>
2018-05-03 10:43:48 +0900

    - 스테이지만 6번째 업로드

--- AccenAlert/AccenAlert/AppDelegate.m
+++ AccenAlert/AccenAlert/AppDelegate.m
@@ -20,6 +20,7 @@
     NSLog(@"세번째  로그");
     NSLog(@"4번째  로그");
     NSLog(@"5번째  로그");
+    NSLog(@"6번째  로그");
 
     // Override point for customization after application launch.
     return YES;
